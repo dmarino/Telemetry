eef354ee78475bcbc2df171aff75b201dfeb3eea
Daniela <d.marino10@uniandes.edu.co>
2020-08-27 13:47:59 -0700

    added legend to heatmap

--- src/components/HeatMap.vue
+++ src/components/HeatMap.vue
@@ -7,8 +7,7 @@
         <h1>GAME HEATMAP</h1>
 
         <div class="legend">
-
-            <span v-for="(item, i) in actionTypes" :key="i">{{item.name}}</span>
+            <color-label v-for="(item, i) in actionTypes" :key="i" :color='item.color' :name='item.name'></color-label>
         </div>
 
         <div class="map-container">
@@ -22,6 +21,7 @@
     import Controller from '@/../lib/controller'
     import BubbleChart from '@/mixins/BubbleChartMixin'
     import Record from '../models/TelemetryRecord';
+    import ColorLabel from './colorLabel';
 
     class HeatMapController extends Controller {
 
@@ -33,10 +33,64 @@
                     responsive: true,
                     maintainAspectRatio: false,
                     legend: { display: false,}
-                }
+                },
+                filter: "",
                 
             }
 
+            this.methods = {
+                
+                fillMap(){
+
+                    var dataS = []
+
+                    this.actionTypes.forEach(type =>{
+
+                        var set = {}
+                        set.backgroundColor = type.color;
+                        set.label = type.name;
+                        set.data = []
+
+                        dataS.push(set);
+                    });
+
+                    this.records.forEach(element => {
+                
+                        dataS.forEach(type =>{
+
+                            if(element.action == type.label){
+                        
+                                var isNew =true;
+
+                                for(var i =0; i< type.data.length && isNew; i++){
+
+
+                                    var dataElement = type.data[i];
+                                    if(dataElement.x == element.posX && dataElement.y == element.posY){
+
+                                        dataElement.r+=5;
+                                        isNew = false;
+                                    }
+                                }
+
+                                if(isNew){
+
+                                    var record = {}
+                                    record.x = element.posX;
+                                    record.y = element.posY;
+                                    record.r =5;
+
+                                    type.data.push(record);
+
+                                }
+                            }
+                        });
+                    });
+
+                    this.chartData = {}
+                    this.chartData.datasets = dataS;
+                }
+            }
             
             this.injectGetters(['records', 'actionTypes']);
             this.injectActions(['loadRecords', 'loadActionTypes']);
@@ -51,38 +105,11 @@
 
         vue_mounted(){
 
-            var dataS = []
-
-            this.actionTypes.forEach(type =>{
-
-                var set = {}
-                set.backgroundColor = type.color;
-                set.label = type.name;
-                set.data = []
-
-                dataS.push(set);
-            });
-
-            this.records.forEach(element => {
-                
-                dataS.forEach(type =>{
-                    if(element.action == type.label){
-                        var record = {}
-                        record.x = element.posX;
-                        record.y = element.posY;
-                        record.r =5;
-
-                        type.data.push(record);
-                    }
-                });
-            });
-
-            this.chartData = {}
-            this.chartData.datasets = dataS;
+            this.fillMap();
         }
     }
 
-    export default new HeatMapController('heat-map',{BubbleChart} );
+    export default new HeatMapController('heat-map',{BubbleChart, ColorLabel} );
 </script>
 
 <style scoped>
@@ -99,6 +126,17 @@
     background-repeat: no-repeat;
     background-position: center;
 
+}
+
+.legend{
+
+    margin: 2%;
+}
+
+.color-label{
+
+    display: inline-block;
+    width: 20%;
 }
 
 </style>
---
+++ src/components/colorLabel.vue
@@ -0,0 +1,44 @@
+<!-- Copyright (c) 2020 Daniela Mariño -->
+
+<template>
+
+    <div class="color-label">
+
+        <div class="elements color-display" :style="style"></br></div>
+        <div class="elements name">{{name}}</div>
+    </div> 
+
+</template>
+
+<script>
+    import Controller from '@/../lib/controller'
+
+    class LabelController extends Controller {
+
+        constructor( name, subComponentList = []) {
+            super( name, subComponentList );
+            this.props = { // props are passed in when using this component
+                name: String,
+                color: String
+            }
+            this.computed= {
+                style () {
+                    return 'background: ' + this.color;
+                }
+            }
+           
+        }
+    }
+
+    export default new LabelController('color-label');
+</script>
+
+<style scoped>
+
+.elements{
+    display: inline-block;
+    margin: 2%;
+    width: 40%;
+    height: 100%;
+}
+</style>
